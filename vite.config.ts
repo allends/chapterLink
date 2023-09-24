3341b2e9dc852fbf9085e77a3b2db16eb3a166b9
allends <allen.davisswing@rutgers.edu>
2023-09-24 12:41:41 -0400

    change the stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import solid from "solid-start/vite";
+import { Options } from "solid-start/vite/plugin";
 import vercel from "solid-start-vercel"
 
 export default defineConfig({
   plugins: [solid({
-    adapter: vercel()
+    adapter: vercel(),
+    islands: false,
   })],
 });
